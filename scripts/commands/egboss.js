d403abc9bd476572dabf780b6b7249e7ab804481
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-24 21:43:06 +0600

    Update egboss.js

--- scripts/commands/egboss.js
+++ scripts/commands/egboss.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	    name: "farhan",
+	    name: "@Rakib Chowdhury",
     version: "1.1.0",
     permission: 0,
     credits: "farhan",
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Farhan")==0 || event.body.indexOf("farhan")==0 || event.body.indexOf("ফারহান")==0 || event.body.indexOf("FARHAN")==0) {
+	if (event.body.indexOf("Farhan")==0 || event.body.indexOf("Rakib")==0 || event.body.indexOf("রাকিব")==0 || event.body.indexOf("FARHAN")==0) {
 		var msg = {
 				body: "ফা্ঁর্ঁহা্ঁন্ঁ এ্ঁখ্ঁন্ঁ খু্ঁব্ঁ বি্ঁজি্ঁ 🥰❤️🥀",
 				attachment: fs.createReadStream(__dirname + `/cache/amiekonbg.mp3`)
