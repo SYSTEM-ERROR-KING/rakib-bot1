51b2099483b1b6615abcfb2734dcdf4a052e6973
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-24 21:51:54 +0600

    Update egboss.js

--- scripts/commands/egboss.js
+++ scripts/commands/egboss.js
@@ -1,27 +1,26 @@
 const fs = require("fs");
 module.exports.config = {
-	    name: "@Rakib Chowdhury",
-    version: "1.1.0",
-    permission: 0,
-    credits: "farhan",
-    description: "noprefix",
-    prefix: true,
-    category: "commands",
-    usages: "farhan",
-    cooldowns: 5,
+	name: "byt",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "VanHung - Fixed by LTD", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "",
+    cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Farhan")==0 || event.body.indexOf("Rakib")==0 || event.body.indexOf("রাকিব")==0 || event.body.indexOf("FARHAN")==0) {
+	if (event.body.indexOf("🤯")==0 || event.body.indexOf("😱")==0 || event.body.indexOf("🙃")==0 || event.body.indexOf("😇")==0) {
 		var msg = {
-				body: "ফা্ঁর্ঁহা্ঁন্ঁ এ্ঁখ্ঁন্ঁ খু্ঁব্ঁ বি্ঁজি্ঁ 🥰❤️🥀",
-				attachment: fs.createReadStream(__dirname + `/cache/amiekonbg.mp3`)
+				body: " ~ কি হয়েছে গো মাথা ঘুরাই নাকি তোমার🥰🥀",
+				attachment: fs.createReadStream(__dirname + `/nayan/cache/amiekonbg.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😽", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	}
