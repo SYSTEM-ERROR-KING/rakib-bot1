88c9501d96f2cbd631f1a4fbc572641d9970c22e
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-25 15:11:45 +0600

    Update RAKIB-4.js

--- scripts/commands/RAKIB-4.js
+++ scripts/commands/RAKIB-4.js
@@ -1,1 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	    name: "fack4",
+    version: "1.1.0",
+    permission: 0,
+    credits: "farhan",
+    description: "noprefix",
+    prefix: true,
+    category: "commands",
+    usages: "😜",
+    cooldowns: 5,
+};
 
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+		if (event.body.indexOf("amogus")==0 || event.body.indexOf("🥱")==0 || event.body.indexOf("🥴")==0 || event.body.indexOf("😎")==0) {
+		var msg = {
+				body: "~  তুমি Attitude দেখাছো, তাতে আমার বাল ছেরা গেলো..!! 😾🥀",
+				attachment: fs.createReadStream(__dirname + `/cache/attitude.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😒", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+      }
