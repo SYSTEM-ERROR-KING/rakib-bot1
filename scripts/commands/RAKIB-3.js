babb9b7aca6aea9be15eff7535928826db101f62
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-25 11:14:56 +0600

    Update RAKIB-3.js

--- scripts/commands/RAKIB-3.js
+++ scripts/commands/RAKIB-3.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	    name: "☺️",
+	    name: "fack2",
     version: "1.1.0",
     permission: 0,
     credits: "farhan",
@@ -15,11 +15,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("amogus")==0 || event.body.indexOf("Amogus")==0 || event.body.indexOf("😛")==0 || event.body.indexOf("😋")==0 || event.body.indexOf("😝")==0) {
 		var msg = {
-				body: "জি্ঁব্বা্ঁ দে্ঁখা্ঁও্ঁ কে্ঁটে্ঁ দি্ঁমু্ঁ 😒🥀",
-				attachment: fs.createReadStream(__dirname + /cache/dakoknogo.mp3)
+				body: "~ আঙ্গুল দেখাও, আঙ্গুল তোমার হেডা দিয়ে ভরে দিম..!!🥀",
+				attachment: fs.createReadStream(__dirname + `/cache/anguldekaw.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😹", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
