3d2f914d233a7f5a414d1ece91197b118784d55c
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-26 09:51:53 +0600

    Create RAKIB-17.js

---
+++ scripts/commands/RAKIB-17.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	    name: "fack17",
+    version: "1.1.0",
+    permission: 0,
+    credits: "farhan",
+    description: "noprefix",
+    prefix: true,
+    category: "commands",
+    usages: "😜",
+    cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+			if (event.body.indexOf("Good morning")==0 || event.body.indexOf("good morning")==0 || event.body.indexOf("gd m9")==0 || event.body.indexOf("Gd M9")==0) {
+		var msg = {
+				body: "~ শুভ সকাল জ্যানু ত্যাহ এবার তো উটো..!!",
+				attachment: fs.createReadStream(__dirname + `/cache/goodmorning.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🍀", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+      }
