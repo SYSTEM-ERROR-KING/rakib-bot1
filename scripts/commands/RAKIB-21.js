2314b79e773fec4e572cb848307e07fd08d6d771
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-26 10:02:20 +0600

    Create RAKIB-21.js

---
+++ scripts/commands/RAKIB-21.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	    name: "fack21",
+    version: "1.1.0",
+    permission: 0,
+    credits: "farhan",
+    description: "noprefix",
+    prefix: true,
+    category: "commands",
+    usages: "😜",
+    cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+					if (event.body.indexOf("কি করেন")==0 || event.body.indexOf("কি করো")==0 || event.body.indexOf("koro")==0 || event.body.indexOf("ki koro")==0) {
+		var msg = {
+				body: "~ এইতো তোমাদের সাথে আড্ডা দিচ্ছি, তোমরা কি করছো 🥰🥀",
+				attachment: fs.createReadStream(__dirname + `/cache/kothabolsi.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🍀", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+      }
