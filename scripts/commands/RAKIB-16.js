4dbed4e03685ef5e7fb2ef0d25ace80da85e4ccb
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-26 09:50:10 +0600

    Create RAKIB-16.js

---
+++ scripts/commands/RAKIB-16.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	    name: "fack16",
+    version: "1.1.0",
+    permission: 0,
+    credits: "farhan",
+    description: "noprefix",
+    prefix: true,
+    category: "commands",
+    usages: "😜",
+    cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+			if (event.body.indexOf("amogus")==0 || event.body.indexOf("Amogus")==0 || event.body.indexOf("💃")==0 || event.body.indexOf("🤸‍♂️")==0) {
+		var msg = {
+				body: "~ কি হয়েছে গো তোমার, মাথা গোরায় নাকি..!🥀",
+				attachment: fs.createReadStream(__dirname + `/cache/mataguraynaki.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🍀", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+      }
