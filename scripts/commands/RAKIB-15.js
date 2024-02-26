82ca10f181da7d2ed399c800b41364f0a94ed34f
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-26 12:46:29 +0600

    Update RAKIB-15.js

--- scripts/commands/RAKIB-15.js
+++ scripts/commands/RAKIB-15.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-			if (event.body.indexOf("Oii")==0 || event.body.indexOf("🙋‍♀️")==0 || event.body.indexOf("oi")==0 || event.body.indexOf("🙋‍♂️")==0) {
+		if (event.body.indexOf("Oii")==0 || event.body.indexOf("🙋‍♀️")==0 || event.body.indexOf("oi")==0 || event.body.indexOf("🙋‍♂️")==0) {
 		var msg = {
 				body: "~ ডাকো কেনো গো, আমি আছি তো এখানে..!✋🥀",
 				attachment: fs.createReadStream(__dirname + `/cache/dakoknogo.mp3`)
