9ba1a718f6de717b9af320a7c1abe47f7c821f4d
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-03-13 16:47:46 +0600

    Update RAKIB-29.js

--- scripts/commands/RAKIB-29.js
+++ scripts/commands/RAKIB-29.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	    name: "fack28",
+	    name: "fac228",
     version: "1.1.0",
     permission: 0,
     credits: "farhan",
@@ -13,9 +13,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
     var { threadID, messageID } = event;
-    if (event.body.indexOf("🤯")==0 || event.body.indexOf("😱")==0 || event.body.indexOf("🙃")==0 || event.body.indexOf("😇")==0) {
-    var msg = {
-        body: " ~ কি হয়েছে গো মাথা ঘুরাই নাকি তোমার🥰🥀",
+     if (event.body.indexOf("🤯")==0 || event.body.indexOf("😱")==0 || event.body.indexOf("🙃")==0 || event.body.indexOf("😇")==0) {
+     var msg = {
+        body: " ~ কি হয়েছে গো মাথা ঘুরাই নাকি, তোমার🥰🥀",
         attachment: fs.createReadStream(__dirname + `/cache/mataguraynaki.mp3`)
       }
 			api.sendMessage( msg, threadID, messageID);
